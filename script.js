e21d68844e6ac4e0a11024dc09992d1b6efc2dc1
Bless <ig.marks@yandex.ru>
2024-05-14 11:29:14 +0300

    finish_js_path

--- script.js
+++ script.js
@@ -1,8 +1,60 @@
 const buttons = document.querySelectorAll('.img_plus')
-console.log(buttons[0].getAttribute('src'))
+const descriptions = document.querySelectorAll('.items')
 
-buttons.forEach((item) => item.addEventListener('click', activeMode))
+buttons.forEach((button) => button.addEventListener('click', activeMode))
 
 function activeMode(e) {
-  console.log(e.target)
+  if (
+    e.target.getAttribute('id') === 'plus_1' &&
+    e.target.getAttribute('src') !== './assets/images/icon-minus.svg'
+  ) {
+    e.target.setAttribute('src', './assets/images/icon-minus.svg')
+    descriptions[0].classList.add('active')
+  } else if (
+    e.target.getAttribute('id') === 'plus_1' &&
+    e.target.getAttribute('src') == './assets/images/icon-minus.svg'
+  ) {
+    e.target.setAttribute('src', './assets/images/icon-plus.svg')
+    descriptions[0].classList.remove('active')
+  }
+  if (
+    e.target.getAttribute('id') === 'plus_2' &&
+    e.target.getAttribute('src') !== './assets/images/icon-minus.svg'
+  ) {
+    e.target.setAttribute('src', './assets/images/icon-minus.svg')
+    descriptions[1].classList.add('active')
+  } else if (
+    e.target.getAttribute('id') === 'plus_2' &&
+    e.target.getAttribute('src') == './assets/images/icon-minus.svg'
+  ) {
+    e.target.setAttribute('src', './assets/images/icon-plus.svg')
+    descriptions[1].classList.remove('active')
+  }
+  if (
+    e.target.getAttribute('id') === 'plus_3' &&
+    e.target.getAttribute('src') !== './assets/images/icon-minus.svg'
+  ) {
+    e.target.setAttribute('src', './assets/images/icon-minus.svg')
+    descriptions[2].classList.add('active')
+  } else if (
+    e.target.getAttribute('id') === 'plus_3' &&
+    e.target.getAttribute('src') == './assets/images/icon-minus.svg'
+  ) {
+    e.target.setAttribute('src', './assets/images/icon-plus.svg')
+    descriptions[2].classList.remove('active')
+  }
+
+  if (
+    e.target.getAttribute('id') === 'plus_4' &&
+    e.target.getAttribute('src') !== './assets/images/icon-minus.svg'
+  ) {
+    e.target.setAttribute('src', './assets/images/icon-minus.svg')
+    descriptions[3].classList.add('active')
+  } else if (
+    e.target.getAttribute('id') === 'plus_4' &&
+    e.target.getAttribute('src') == './assets/images/icon-minus.svg'
+  ) {
+    e.target.setAttribute('src', './assets/images/icon-plus.svg')
+    descriptions[3].classList.remove('active')
+  }
 }
